319f0ecf8e8cc4af0eb31ea465dda9ba2fe08036
brad <developer2@bubblynet.com>
2022-07-30 18:31:30 -0400

    neovim from release

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
+git submodule update --init
+
 # Latest neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Expose nvim globally
+mv squashfs-root /
+ln -s /squashfs-root/AppRun /usr/bin/nvim
+
 
 # Env depends
-apt install -y curl ripgrep xsel wl-clipboard tmux dconf-editor python3-pynvim git
+sudo apt install -y curl ripgrep xsel wl-clipboard tmux dconf-editor python3-pynvim git npm python3-venv
+
+npm i -g neovim
 
 # Copy over tmux config
 cp .tmux.conf ~/.tmux.conf
@@ -18,7 +28,7 @@ cd ..
 
 # Copy over nvim config
 mkdir ~/.config/nvim
-cp -r nvim-basic-ide ~/.config/nvim
+cp -R nvim-basic-ide/. ~/.config/nvim
 
 # Copy over terminal config
 dconf load /org/gnome/terminal/legacy/profiles:/:697efb20-2b3c-4387-9c30-afb2f2ca3769/ < solar-term.dconf
