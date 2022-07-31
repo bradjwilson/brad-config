9ab22c89a72c28466fb23749ddd6c30136d76321
Brad Wilson <developer2@bubblynet.com>
2022-07-31 08:53:54 -0400

    mac script install working

--- install_mac.sh
+++ install_mac.sh
@@ -17,24 +17,13 @@ git apply ../nvim-basic.patch
 cd ..
 
 # Copy over nvim config
-mkdir ~/.config/nvim
+sudo rm -r ~/.config/nvim
 cp -R nvim-basic-ide/. ~/.config/nvim
 
-exit(0)
-
 # Download nerd font
-mkdir -p ~/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip
-unzip Ubuntu.zip -d ~/.local/share/fonts/
-fc-cache -fv
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
 
-# Source the fonts for current and all sessions
-wget https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/bin/scripts/lib/i_linux.sh
-mv i_linux.sh ~/.local/share/fonts/i_linux.sh
-source ~/.local/share/fonts/i_linux.sh
-
-# TODO check if already in bachrc
-echo "source ~/.local/share/fonts/i_linux.sh" >> ~/.bashrc
 
 
 
