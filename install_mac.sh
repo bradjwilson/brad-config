3c5cc06e12241881816c3be4aab9d6ffa6966e00
Brad Wilson <brad.wilson10@yahoo.com>
2022-07-30 20:57:08 -0400

    WIP mac install

--- install_mac.sh
+++ install_mac.sh
@@ -3,20 +3,10 @@
 git submodule update --init
 
 # Latest neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-./squashfs-root/AppRun --version
+curl -LO https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-macos.tar.gz
+tar xzvf nvim-macos.tar.gz
 
-# Expose nvim globally
-mv squashfs-root /
-ln -s /squashfs-root/AppRun /usr/bin/nvim
-
-
-# Env depends
-sudo apt install -y curl ripgrep xsel wl-clipboard tmux dconf-editor python3-pynvim git npm python3-venv
-
-npm i -g neovim
+pip3 install pynvim
 
 # Copy over tmux config
 cp .tmux.conf ~/.tmux.conf
@@ -30,8 +20,7 @@ cd ..
 mkdir ~/.config/nvim
 cp -R nvim-basic-ide/. ~/.config/nvim
 
-# Copy over terminal config
-dconf load /org/gnome/terminal/legacy/profiles:/:697efb20-2b3c-4387-9c30-afb2f2ca3769/ < solar-term.dconf
+exit(0)
 
 # Download nerd font
 mkdir -p ~/.local/share/fonts
