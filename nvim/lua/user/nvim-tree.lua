6137203cfd6e16694d6dc345db5b6acb1d5f2694
Brad Wilson <brad.wilson10@yahoo.com>
2023-08-30 14:55:40 -0400

    update toggle term

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -3,18 +3,12 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
 local function on_attach(bufnr)
   local api = require('nvim-tree.api')
 
   local function opts(desc)
     return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
-
 
   -- Default mappings. Feel free to modify or remove as you wish.
   --
@@ -85,7 +79,6 @@ local function on_attach(bufnr)
 
 end
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
     on_attach = on_attach,
@@ -138,13 +131,6 @@ nvim_tree.setup {
   view = {
     width = 35,
     side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
   },
 }
 
